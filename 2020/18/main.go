fda2dd091c2ed8af5477ab3b7aec1b6306df5851
Adam Thunberg <thadam@student.chalmers.se>
2021-06-14 21:27:00 +0200

    The ugliest of the ugly.

--- 2020/18/main.go
+++ 2020/18/main.go
@@ -4,8 +4,9 @@ import "fmt"
 import "bufio"
 import "os"
 import "log"
-// import "strconv"
-// import "strings"
+// import "regexp"
+import "strconv"
+import "strings"
 
 func LinesInFile(fileName string) []string {
 	f, err := os.Open(fileName)
@@ -67,14 +68,107 @@ func Eval(ex []rune) int {
 	return result
 }
 
-func main() {
-	lines := LinesInFile("input")
+func simplify(chars []string, operator string) []string {
+	for i, char := range chars {
+		if char == operator {
+			replace := i - 1
+			a, _ := strconv.Atoi(chars[replace])
+			b, _ := strconv.Atoi(chars[replace + 2])
+			new := []string{}
+			new = append(chars[:replace], chars[replace + 2:]...)
+			sum := ""
+			if operator == "+" {
+				sum = strconv.Itoa(a + b)
+			} else {
+				sum = strconv.Itoa(a * b)
+			}
 
+			new[replace] = sum
+
+			return new
+		}
+	}
+
+	return []string{}
+}
+
+func Eval2(ex []rune) int {
+	result := 0
+
+	// simplifyplus(ex)
+
+	return result
+}
+
+func main() {
 	sum := 0
 
-	for _, line := range lines {
+	for _, line := range LinesInFile("input") {
 		sum += Eval([]rune(line))
 	}
 
 	fmt.Println("Part 1:", sum)
+
+	sum = 0
+	for _, line := range LinesInFile("input") {
+		lastOpening := 0
+		i := 0
+
+		for {
+			if i >= len(line) {
+				if lastOpening == 0 {
+					break
+				}
+				lastOpening = 0
+				i = 0
+			}
+			char := line[i]
+
+			if char == '(' {
+				lastOpening = i
+			} else if char == ')' {
+				tmp := line[lastOpening + 1:]
+				tmp = tmp[:i - lastOpening - 1]
+
+				new := strings.Split(tmp, " ")
+
+				fmt.Println(tmp)
+
+				for _, op := range []string{"+", "*"} { for {
+					result := simplify(new, op)
+
+					if len(result) == 0 {
+						break
+					}
+
+					new = result
+				}}
+
+				line = line[:lastOpening] + new[0] + line[i + 1:]
+
+				fmt.Println(line)
+				lastOpening = 0
+				i = 0
+			}
+
+			i++
+		}
+
+		new := strings.Split(line, " ")
+		for _, op := range []string{"+", "*"} { for {
+			result := simplify(new, op)
+
+			if len(result) == 0 {
+				break
+			}
+
+			new = result
+		}}
+
+
+		num, _ := strconv.Atoi(new[0])
+		sum += num
+	}
+
+	fmt.Println("Part 2: ", sum)
 }
